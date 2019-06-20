68513b6ded7493eec2606d6daf3a082726a11873
Akshit Ostwal <akshitostwal@gmail.com>
2019-06-20 15:45:43 +0530

    theme added

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -4,7 +4,7 @@ import './products.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
-  ProductManager(this.startingProduct){
+  ProductManager({this.startingProduct = 'Sweets Tester'}){
     print('[productmanager widget] Constructor');
   }
 
@@ -37,6 +37,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).primaryColor,
             onPressed: () {
               setState(() {
                 _products.add('Advanced food tester');
