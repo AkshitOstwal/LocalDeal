c7e5a427d4e232dc7b7ae44f963f2b80a0fda85d
Akshit Ostwal <akshitostwal@gmail.com>
2019-06-29 12:20:44 +0530

    fully added working tabs

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,36 +1,56 @@
+import './product_create.dart';
+import './product_list.dart';
 import './products.dart';
+
 import 'package:flutter/material.dart';
 
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text("Choose"),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text("Choose"),
+                ),
+                ListTile(
+                  title: Text("All Products"),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => ProductsPage()));
+                  },
+                )
+              ],
             ),
-            ListTile(
-              title: Text("All Products"),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductsPage()));
-              },
-            )
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text("Manage Products"),
-      ),
-      body: Center(
-        child: Text('Manage your Products here'),
-      ),
-    );
+          ),
+          appBar: AppBar(
+            title: Text("Manage Products"),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: "Create Product",
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: "MY Product",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+						children: <Widget>[
+							ProductCreatePage(),
+							ProductListPage()
+						],
+					)
+        ));
   }
 }
