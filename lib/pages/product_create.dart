bff56101b4eb3eec99db60c53a97d7d5017a5f29
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-03 15:32:14 +0530

    added a modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Center(
-      child: Text("Create a Product"),
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text('This is a modal'),
+                );
+              });
+        },
+      ),
     );
   }
 }
