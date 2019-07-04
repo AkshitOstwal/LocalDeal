c6e06612a214c4a499be17eb2b3ded4d8afb1549
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-04 13:13:07 +0530

    added textfield and storing value from them

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('This is a modal'),
-                );
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Title',
+              hintText: 'Enter Tilte of Product',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
               });
-        },
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Description',
+              hintText: 'Enter Description of product',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Price',
+              hintText: 'Enter Price',
+            ),
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+            keyboardType: TextInputType.number,
+          )
+        ],
       ),
     );
   }
