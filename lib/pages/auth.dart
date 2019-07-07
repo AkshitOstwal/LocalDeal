c7df36cbf82deedd254a9f7192faf252417df59d
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-07 17:09:38 +0530

    optimized auth page with validation

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -8,66 +8,80 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _emailValue;
-  String _passwordValue;
-  bool _acceptTerms = false;
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   DecorationImage _buildBackgroundImage() {
     return DecorationImage(
       image: AssetImage('assets/background.jpg'),
       fit: BoxFit.cover,
       colorFilter:
-          ColorFilter.mode(Colors.grey.withOpacity(0.4), BlendMode.dstATop),
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
     );
   }
 
   Widget _buildEmailTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'E-Mail',
         filled: true,
         fillColor: Colors.white.withOpacity(0.7),
       ),
       keyboardType: TextInputType.emailAddress,
-      onChanged: (String value) {
-        setState(() {
-          _emailValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                .hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (String value) {
+        _formData['email'] = value;
       },
     );
   }
 
   Widget _buildPasswordTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'Password',
         filled: true,
         fillColor: Colors.white.withOpacity(0.7),
       ),
       obscureText: true,
-      onChanged: (String value) {
-        setState(() {
-          _passwordValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Password invalid';
+        }
+      },
+      onSaved: (String value) {
+        _formData['password'] = value;
       },
     );
   }
 
   Widget _buildAcceptSwitch() {
     return SwitchListTile(
-      value: _acceptTerms,
+      value: _formData['acceptTerms'],
       onChanged: (bool value) {
         setState(() {
-          _acceptTerms = value;
+          _formData['acceptTerms'] = value;
         });
       },
       title: Text('Accept Terms and Condition'),
     );
   }
 
-  void _subitForm() {
-    print(_emailValue);
-    print(_passwordValue);
+  void _submitForm() {
+    if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
+      return;
+    }
+    _formKey.currentState.save();
+    print(_formData);
     Navigator.pushReplacementNamed(context, '/products');
   }
 
@@ -75,7 +89,10 @@ class _AuthPageState extends State<AuthPage> {
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550.0 ? 500 : deviceWidth * 0.95;
-    return Scaffold(
+    return GestureDetector(onTap: (){
+      FocusScope.of(context).requestFocus(FocusNode());
+    },
+        child: Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
@@ -87,32 +104,32 @@ class _AuthPageState extends State<AuthPage> {
         child: Center(
           child: SingleChildScrollView(
             child: Container(
-              width: targetWidth,
-              child: Column(
-                children: <Widget>[
-                  _buildEmailTextField(),
-                  SizedBox(
-                    height: 11.0,
+                width: targetWidth,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      _buildEmailTextField(),
+                      SizedBox(
+                        height: 11.0,
+                      ),
+                      _buildPasswordTextField(),
+                      _buildAcceptSwitch(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        child: Text("LOGIN"),
+                        onPressed: _submitForm,
+                      ),
+                    ],
                   ),
-                  _buildPasswordTextField(),
-                  _buildAcceptSwitch(),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Center(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text("LOGIN"),
-                      onPressed: _subitForm,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                )),
           ),
         ),
       ),
-    );
+    ));
   }
 }
