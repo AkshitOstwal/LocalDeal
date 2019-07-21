025531555a0999248642009021c6d6f250679b53
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-21 23:14:42 +0530

    added location.dart not working

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -1,44 +1,44 @@
-import 'package:map_view/map_view.dart';
-import 'package:flutter/material.dart';
+// import 'package:map_view/map_view.dart';
+// import 'package:flutter/material.dart';
 
-class LocationInput extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _LocationInputState();
-  }
-}
+// class LocationInput extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _LocationInputState();
+//   }
+// }
 
-class _LocationInputState extends State<LocationInput> {
-  Uri _staticMapUri ; 
-  @override
-  void initState() {
-    super.initState();
-  }
+// class _LocationInputState extends State<LocationInput> {
+//   Uri _staticMapUri ; 
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 
-  void getStaticMap() {
-    final StaticMapProvider staticMapviewProvider =
-        StaticMapProvider('aAIzaSyDTDdtXLuAoEZHfG3RZ91sfspQUoEuqGsY');
-    final Uri staticMapUri = staticMapviewProvider.getStaticUriWithMarkers(
-        [Marker('position', 'Position', 41.40338, 2.17403)],
-        center: Location(41.40338, 2.17403),
-        width: 500,
-        height: 300,
-        maptype: StaticMapViewType.roadmap);
-        setState(() {
-         _staticMapUri = staticMapUri; 
-        });
-  }
+//   void getStaticMap() {
+//     final StaticMapProvider staticMapviewProvider =
+//         StaticMapProvider('aAIzaSyDTDdtXLuAoEZHfG3RZ91sfspQUoEuqGsY');
+//     final Uri staticMapUri = staticMapviewProvider.getStaticUriWithMarkers(
+//         [Marker('position', 'Position', 41.40338, 2.17403)],
+//         center: Location(41.40338, 2.17403),
+//         width: 500,
+//         height: 300,
+//         maptype: StaticMapViewType.roadmap);
+//         setState(() {
+//          _staticMapUri = staticMapUri; 
+//         });
+//   }
 
-  void updateLocation() {}
+//   void updateLocation() {}
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      TextFormField(),
-      SizedBox(
-        height: 10,
-      ),
-      Image.network(_staticMapUri.toString())
-    ]);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(children: <Widget>[
+//       TextFormField(),
+//       SizedBox(
+//         height: 10,
+//       ),
+//       Image.network(_staticMapUri.toString())
+//     ]);
+//   }
+// }
