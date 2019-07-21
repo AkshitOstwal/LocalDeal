5d14bfdd702fc856e33ecd385ee75be603ad03bf
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-22 00:32:56 +0530

    getting image with camera and gallayr and saving it

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -8,13 +11,68 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageeInputstate extends State<ImageInput> {
+  File _imageFile;
+
+  void _getImage(BuildContext context, ImageSource source) {
+    ImagePicker.pickImage(source: source, maxWidth: 400).then((File image) {
+      setState(() {
+        _imageFile = image;
+      });
+      Navigator.pop(context);
+    });
+  }
+
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            padding: EdgeInsets.all(10),
+            height: 150,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Pick an Image',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                FlatButton(
+                  child: Text(
+                    'Use Camera',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  onPressed: () {
+                    _getImage(context, ImageSource.camera);
+                  },
+                ),
+                FlatButton(
+                  child: Text(
+                    'Use Gallery',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  ),
+                  onPressed: () {
+                    _getImage(context, ImageSource.gallery);
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     final buttonColor = Theme.of(context).primaryColor;
     return Column(
       children: <Widget>[
         OutlineButton(
-          onPressed: () {},
+          onPressed: () {
+            _openImagePicker(context);
+          },
           borderSide: BorderSide(color: buttonColor, width: 1),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -29,7 +87,19 @@ class _ImageeInputstate extends State<ImageInput> {
               )
             ],
           ),
-        )
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        _imageFile == null
+            ? Text('Please pick an image.')
+            : Image.file(
+                _imageFile,
+                height: 300.0,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+              )
       ],
     );
   }
