b8cbffd74cdd94e4e5b6745eb7dc91729f0ba2d3
Akshit Ostwal <akshitostwal@gmail.com>
2019-07-26 20:23:54 +0530

    added app icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,7 @@ class _MyAppState extends State<MyApp> {
     return ScopedModel<MainModel>(
       model: _model,
       child: MaterialApp(
+        title: 'EasyList',
         // debugShowMaterialGrid: true,
         theme: getAdaptiveThemeData(context),
         // home: AuthPage(),
